9904cc21679bc04015c9994a3173e519a224a3f7
Miguel Guedes de Oliveira <miguelguedes.oli@gmail.com>
2025-05-26 17:47:29 -0300

    Condicionais

---
+++ src/Main.java
@@ -0,0 +1,87 @@
+import java.util.Scanner;
+import java.util.SortedMap;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        //Verificando se o um número pe par ou ímpar
+        /*int n = sc.nextInt();
+        sc.close();
+
+        if (n % 2 == 0) {
+            System.out.println("O numero é par");
+        } else {
+            System.out.println("O numero é impar");
+        }*/
+
+        //Verificando aprovação em uma disciplina
+        /*double primeiraNota = 7.9;
+        double segundaNota = 6.0;
+
+        double media = (primeiraNota + segundaNota) / 2;
+
+        if (media >= 7.0) {
+            System.out.printf("O estudante teve média %.2f e foi aprovado", media);
+        } else if (media >= 5.0) {
+            System.out.printf("O estudante teve média %.2f e esta em recuperação", media);
+        } else {
+            System.out.printf("O estudante teve média %.2f e foi reprovado", media);
+        }*/
+
+        //Validação de senha
+
+        /*String senhaCorreta = "123456";
+        System.out.println("Digite sua senha:");
+
+        String tentativaSenha = sc.nextLine();
+        sc.close();
+
+        if (tentativaSenha.equals(senhaCorreta)) {
+            System.out.println("Senha correta!");
+        } else {
+            System.out.println("Senha incorreta!");
+        }*/
+
+        //comparação de dois números
+
+        /*System.out.println("Digite o primeiro numero: ");
+        int primeiroNumero = sc.nextInt();
+        System.out.println("Digite o segundo numero: ");
+        int segundoNumero = sc.nextInt();
+
+        sc.close();
+
+        if (primeiroNumero > segundoNumero) {
+            System.out.println("O maior nuúmero é " + primeiroNumero);
+        } else if (primeiroNumero < segundoNumero) {
+            System.out.println("O maior número é " + segundoNumero);
+        } else {
+            System.out.println("Os números são iguais");
+        }*/
+
+        //Desconto em compras
+
+        /*System.out.println("Digite o valor da sua compra:");
+        double valor = sc.nextDouble();
+        double porcentagem = (double) 10 / 100;
+
+        double valorDesconto = valor - (valor * porcentagem);
+
+        if (valor >= 100.0) {
+            System.out.println("Desconto de 10% aplicado!");
+            System.out.println("Novo valor: " + valorDesconto);
+
+        } else {
+            System.out.println("Nenhum desconto aplicado.");
+            System.out.println("Valor total: " + valor);
+        }*/
+
+
+
+
+
+    }
+}
